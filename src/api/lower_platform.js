2bc78152375286e93b50b388de90cf2d974b6bc7
hilanmiao <hilanmiao@126.com>
2018-02-02 17:31:08 +0800

    设备管理，首页websocket测试

---
+++ src/api/lower_platform.js
@@ -0,0 +1,43 @@
+import request from '@/utils/request'
+
+// 获取列表数据
+export function getListEnquipment(params) {
+  return request({
+    url: 'v1/dpfInfo/dpfInfos',
+    method: 'get',
+    params
+  })
+}
+// 创建
+export function postModelEnquipment(data) {
+  return request({
+    url: 'v1/dpfInfo/dpfInfo',
+    method: 'post',
+    data
+  })
+}
+
+// 修改
+export function putModelEnquipment(data) {
+  return request({
+    url: 'v1/dpfInfo/dpfInfo',
+    method: 'post',
+    data
+  })
+}
+
+// 删除
+export function deleteModelEnquipment(id) {
+  return request({
+    url: 'v1/dpfInfo/' + id,
+    method: 'delete'
+  })
+}
+
+// 获取详情
+export function getModelEnquipment(id) {
+  return request({
+    url: 'v1/dpfInfo/' + id,
+    method: 'get'
+  })
+}
